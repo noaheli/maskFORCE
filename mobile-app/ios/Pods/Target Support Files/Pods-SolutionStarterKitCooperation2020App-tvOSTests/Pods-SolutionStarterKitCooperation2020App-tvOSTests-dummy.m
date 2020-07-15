1f356bcbe36cbbfd6950d58af1fabaf4564ac6ee
Tiffany Zhu <tiffanyzhu19@gmail.com>
2020-07-15 10:59:10 -0400

    Add starter kit code

---
+++ mobile-app/ios/Pods/Target Support Files/Pods-SolutionStarterKitCooperation2020App-tvOSTests/Pods-SolutionStarterKitCooperation2020App-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SolutionStarterKitCooperation2020App_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_SolutionStarterKitCooperation2020App_tvOSTests
+@end
